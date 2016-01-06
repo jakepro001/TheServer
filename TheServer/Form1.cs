daf9dab7597f59a15faba2ea57d0a4f9a3137309
ep sooraj <epsooraj4@gmail.com>
2016-01-06 16:39:05 +0530

    The Server

--- TheServer/Form1.cs
+++ TheServer/Form1.cs
@@ -3,7 +3,9 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
+using System.Net.Sockets;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
@@ -12,9 +14,249 @@ namespace TheServer
 {
     public partial class Form1 : Form
     {
+
+        public delegate void invokeDelegate();
         public Form1()
         {
             InitializeComponent();
         }
+
+
+        
+
+        /// <summary>
+        /// Client Handling
+        /// </summary>
+        #region Client/Information Handle
+        private void send()
+        {
+            string data = "";
+
+            foreach (string line in txtText.Lines)
+            {
+                data = data + line.Replace("\r", "").Replace("\n", "") + "\r\n";
+            }
+            data = data.Substring(0, data.Length - 2);
+
+            tcpServer1.Send(data);
+
+            logData(true, data);
+        }
+
+        protected byte[] readStream(TcpClient client)
+        {
+            NetworkStream stream = client.GetStream();
+            if (stream.DataAvailable)
+            {
+                byte[] data = new byte[client.Available];
+
+                int bytesRead = 0;
+                try
+                {
+                    bytesRead = stream.Read(data, 0, data.Length);
+                }
+                catch (IOException)
+                {
+                }
+
+                if (bytesRead < data.Length)
+                {
+                    byte[] lastData = data;
+                    data = new byte[bytesRead];
+                    Array.ConstrainedCopy(lastData, 0, data, 0, bytesRead);
+                }
+                return data;
+            }
+            return null;
+        }
+
+        private void tcpServer1_OnDataAvailable(tcpServer.TcpServerConnection connection)
+        {
+            byte[] data = readStream(connection.Socket);
+
+            if (data != null)
+            {
+                string dataStr = Encoding.ASCII.GetString(data);
+
+                invokeDelegate del = () =>
+                {
+                    logData(false, dataStr);
+                };
+                Invoke(del);
+
+                data = null;
+            }
+        }
+
+        #endregion
+
+
+        /// <summary>
+        /// Server Side
+        /// </summary>
+        #region The Server
+
+        private void openTcpPort()
+        {
+            tcpServer1.Close();
+            tcpServer1.Port = Convert.ToInt32(txtPort.Text);
+            txtPort.Text = tcpServer1.Port.ToString();
+            tcpServer1.Open();
+
+            displayTcpServerStatus();
+        }       
+
+        private void tcpServer1_OnConnect(tcpServer.TcpServerConnection connection)
+        {
+            invokeDelegate setText = () => lblConnected.Text = tcpServer1.Connections.Count.ToString();
+
+            Invoke(setText);
+        }
+
+        #endregion
+
+
+
+        /// <summary>
+        /// The Contols For UI
+        /// </summary>
+        #region UI Control
+
+        private void displayTcpServerStatus()
+        {
+            if (tcpServer1.IsOpen)
+            {
+                lblStatus.Text = "PORT OPEN";
+                lblStatus.BackColor = Color.Lime;
+            }
+            else
+            {
+                lblStatus.Text = "PORT NOT OPEN";
+                lblStatus.BackColor = Color.Red;
+            }
+        }
+
+        public void logData(bool sent, string text)
+        {
+            txtLog.Text += "\r\n" + DateTime.Now.ToString("yyyy/MM/dd HH:mm:ss tt") + (sent ? " SENT:\r\n" : " RECEIVED:\r\n");
+            txtLog.Text += text;
+            txtLog.Text += "\r\n";
+            if (txtLog.Lines.Length > 500)
+            {
+                string[] temp = new string[500];
+                Array.Copy(txtLog.Lines, txtLog.Lines.Length - 500, temp, 0, 500);
+                txtLog.Lines = temp;
+            }
+            txtLog.SelectionStart = txtLog.Text.Length;
+            txtLog.ScrollToCaret();
+        }
+        #endregion
+
+
+        /// <summary>
+        /// The Event Handlers
+        /// 
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        #region Event Handlers
+
+        private void btnChangePort_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                openTcpPort();
+            }
+            catch (FormatException)
+            {
+                MessageBox.Show("Port must be an integer", "Invalid Port", MessageBoxButtons.OK, MessageBoxIcon.Error, MessageBoxDefaultButton.Button1);
+            }
+            catch (OverflowException)
+            {
+                MessageBox.Show("Port is too large", "Invalid Port", MessageBoxButtons.OK, MessageBoxIcon.Error, MessageBoxDefaultButton.Button1);
+            }
+        }
+
+        private void btnSend_Click(object sender, EventArgs e)
+        {
+            send();
+        }
+
+        private void btnClose_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        private void frmMain_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            tcpServer1.Close();
+        }
+
+        private void frmMain_Load(object sender, EventArgs e)
+        {
+            btnChangePort_Click(null, null);
+
+            timer1.Enabled = true;
+        }
+
+        private void frmMain_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            timer1.Enabled = false;
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            displayTcpServerStatus();
+            lblConnected.Text = tcpServer1.Connections.Count.ToString();
+        }        
+
+        private void txtIdleTime_TextChanged(object sender, EventArgs e)
+        {
+            try
+            {
+                int time = Convert.ToInt32(txtIdleTime.Text);
+                tcpServer1.IdleTime = time;
+            }
+            catch (FormatException) { }
+            catch (OverflowException) { }
+        }
+
+        private void txtMaxThreads_TextChanged(object sender, EventArgs e)
+        {
+            try
+            {
+                int threads = Convert.ToInt32(txtMaxThreads.Text);
+                tcpServer1.MaxCallbackThreads = threads;
+            }
+            catch (FormatException) { }
+            catch (OverflowException) { }
+        }
+
+        private void txtAttempts_TextChanged(object sender, EventArgs e)
+        {
+            try
+            {
+                int attempts = Convert.ToInt32(txtAttempts.Text);
+                tcpServer1.MaxSendAttempts = attempts;
+            }
+            catch (FormatException) { }
+            catch (OverflowException) { }
+        }
+
+        private void txtValidateInterval_TextChanged(object sender, EventArgs e)
+        {
+            try
+            {
+                int interval = Convert.ToInt32(txtValidateInterval.Text);
+                tcpServer1.VerifyConnectionInterval = interval;
+            }
+            catch (FormatException) { }
+            catch (OverflowException) { }
+        }
+
+
+        #endregion
+
+
     }
 }
